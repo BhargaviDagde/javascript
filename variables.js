25d93ecd2b0c55c5efb636ce7230651a945bf626
BhargaviDagde <168635789+BhargaviDagde@users.noreply.github.com>
2025-06-15 04:55:28 +0000

    variables

---
+++ variables.js
@@ -0,0 +1,14 @@
+const accountId=144553;//cannot be changed
+let accountEmail="bhargavi@google.com"//space reserved in memory
+var accountPassword= "12345"
+accoutCity= "Jaipur"//shouldnt be done like this
+let accountState
+//accountId=2
+accountEmail="bhvbjk"
+accountPassword="7374648"
+accoutCity="bengaluru"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accoutCity],accountState)
+/*prefer not to use var
+because of issue in block scope and functional scope*/
+
