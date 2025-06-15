4cbdb64ac65f89d86daa03b5f00d11a0f926cc5e
BhargaviDagde <168635789+BhargaviDagde@users.noreply.github.com>
2025-06-15 05:24:52 +0000

    conversions

---
+++ conversions.js
@@ -0,0 +1,8 @@
+//let score="33ab"
+//console.log(typeof score)
+//let valueInNumber= Number(score)
+//console.log(typeof valueInNumber)
+let loggedIn=1 
+//1 is true and 0 is false
+let booleanIsLoggedin= Boolean(loggedIn)
+console.log(booleanIsLoggedin);
