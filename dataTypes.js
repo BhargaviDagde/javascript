55793ca9175ddc3e94f6076757db175fe6c46893
BhargaviDagde <168635789+BhargaviDagde@users.noreply.github.com>
2025-06-15 04:56:43 +0000

    datatypes

---
+++ dataTypes.js
@@ -0,0 +1,12 @@
+"use strict";//treat all js code as newer version
+//alert() we are using node js not browser
+//for notes refer mdn
+let name="bhargavi"//string
+let age=34//number
+let isLoggedIn=false//boolean
+//null => is a standalone value
+//undefined =>
+//symbol=>uniqueness
+//object
+console.log(typeof"bhargavi");
+console.log(typeof null);
